1703fc7d44b9d450342d98e00e0744ec6cf6e714
benjamonnguyen <ben85629@gmail.com>
2023-08-17 21:05:53 -0700

    extract processExtensions() and class Extension to extension.ts

--- src/model/TodoItem.ts
+++ src/model/TodoItem.ts
@@ -2,9 +2,8 @@ import { Item } from "jstodotxt";
 import { randomUUID } from "crypto";
 import { ActionButton, ActionType, type ViewModel } from ".";
 import { AddModal, EditItemModal } from "src/component";
-import { moment, Notice } from "obsidian";
-import { Moment } from "moment";
-import MyPlugin from "src/main";
+import { moment } from "obsidian";
+import { processExtensions, Extension } from "src/extension";
 
 
 export default class TodoItem extends Item implements ViewModel {
@@ -19,7 +18,7 @@ export default class TodoItem extends Item implements ViewModel {
 
     setBody(body: string): void {
         super.setBody(body);
-        this.processExtensions();
+        processExtensions(this);
         if (!this.created()) {
             this.setCreated(new Date());
         }
@@ -46,32 +45,7 @@ export default class TodoItem extends Item implements ViewModel {
             });
         }
         
-        let descriptionHtml = "<span class=\"todotxt-item-description\">";
-        for (const str of this.body().split(" ")) {
-            if (str.startsWith(Extension.DUE + ":")) {
-                const split = str.split(":");
-                const due = moment(split.at(1));
-                const now = moment();
-                const extension = split[0] + ":" + split[1];
-                if (due.isSame(now, "d")) {
-                    descriptionHtml += "<span class=\"todotxt-due-today\">" + extension + "</span>";
-                } else if (due.isBefore(now, "d")) {
-                    descriptionHtml += "<span class=\"todotxt-overdue\">" + extension + "</span>";
-                } else {
-                    descriptionHtml += "<span class=\"todotxt-due\">" + extension + "</span>";
-                }
-                const remaining = split.slice(2);
-                if (remaining.length) {
-                    descriptionHtml += ":" + split.slice(2).join(":");
-                }
-                descriptionHtml += " ";
-            } else {
-                descriptionHtml += str + " ";
-            }
-        };
-        descriptionHtml += "</span>";
-        const description = item.createSpan();
-        description.outerHTML = descriptionHtml.trimEnd();
+        item.createEl("span").outerHTML = this.buildDescriptionHtml();
 
         const actions = item.createSpan({
             cls: "todotxt-item-actions"
@@ -101,89 +75,13 @@ export default class TodoItem extends Item implements ViewModel {
         }
     }
 
-    // #region extensions
     addExtension(key: string, value: string): void {
         if (Extension.isReserved(key) && this.extensions().find(ext => ext.key === key)) {
             throw "Extension already exists for key: " + key;
         }
         super.addExtension(key, value);
-        this.processExtensions();
+        processExtensions(this);
     }
-
-    private processExtensions() {
-
-        this.processDueExtension();
-    }
-
-    private processDueExtension() {
-        const extensions = this.extensions().filter(ext => ext.key === Extension.DUE);
-        for (let i = 1; i < extensions.length; i++) {
-            const { key, value } = extensions[i];
-            this.removeExtension(key, value)
-            console.warn(`Removed duplicate key/value pair for reserved extension: ${key}/${value}`);
-        }
-        const first = extensions.first();
-        // <YYYY-MM-DD> (ex. 1996-08-06)
-        let res: Moment = moment(first?.value, "YYYY-MM-DD", true);
-        if (first && !res.isValid()) {
-            if (!res.isValid()) {
-                // <MM-DD> (ex. 08-06)
-                res = moment(first.value, "MM-DD", true);
-            }
-            const dateCalculationDetails: string[] = [];
-            if (!res.isValid()) {
-                // <number><[dateUnit]> (ex. 1d)
-                // dateUnits: d, w, m, y
-                // if only number is provided, unit is days (ex. 0 = today)
-                const d = /^\d+$/.exec(first.value)?.first() || /(\d+)d/.exec(first.value)?.at(1);
-                const w = /(\d+)w/.exec(first.value)?.at(1);
-                const m = /(\d+)m/.exec(first.value)?.at(1);
-                const y = /(\d+)y/.exec(first.value)?.at(1);
-                // <dayOfWeek>
-                // M, Tu, W, Th, F, Sa, Su
-                // dayOfWeek must be at the very beginning or end
-                const dayOfWeek = /(^(?:M|Tu|W|Th|F|Sa|Su)|(?:M|Tu|W|Th|F|Sa|Su)$)/.exec(first.value)?.at(1);
-
-                // can be combined
-                // 1w2d = 9 days (1 week + 2 days)
-                // 2mM = first Monday in 2 months
-                // M2m = first Monday in 2 months
-                if (d || w || m || y || dayOfWeek) {
-                    res = moment()
-                        .add(d, "d")
-                        .add(w, "w")
-                        .add(m, "M")
-                        .add(y, "y");
-
-                    if (dayOfWeek){
-                        if (!res.isValid()) res = moment();
-                        res.day(["Su", "M", "Tu", "W", "Th", "F", "Sa"].indexOf(dayOfWeek) + 7);
-                        dateCalculationDetails.push("dayOfWeek: " + dayOfWeek);
-                    }
-                    if (d) dateCalculationDetails.push("d: " + d);
-                    if (w) dateCalculationDetails.push("w: " + w);
-                    if (m) dateCalculationDetails.push("m: " + m);
-                    if (y) dateCalculationDetails.push("y: " + y);
-                }
-            }
-
-            if (res.isValid()) {
-                const dueDate = res.format("YYYY-MM-DD");
-                this.setExtension(first.key, dueDate);
-                let msg = "Due date set to " + dueDate;
-                if (dateCalculationDetails.length) {
-                    msg += `\n(${dateCalculationDetails.join(", ")})`;
-                }
-                new Notice(MyPlugin.NAME + " INFO\n" + msg, 15000);
-            } else {
-                const errMsg = "Invalid value for due extension: " + first.value;
-                console.warn(errMsg);
-                this.removeExtension(first.key, first.value);
-                new Notice(MyPlugin.NAME + " WARNING\n" + errMsg, 15000);
-            }
-        }
-    }
-    // #endregion
     
     private getPriorityHtmlClasses(): string[] {
         let letterCls;
@@ -203,14 +101,33 @@ export default class TodoItem extends Item implements ViewModel {
         
         return [letterCls, "todotxt-priority"];
     }
-}
 
-class Extension {
-    static DUE = "due";
-    static RECURRING = "rec";
-
-    static isReserved(extension: string): boolean {
-        const reserved = new Set([Extension.DUE, Extension.RECURRING]);
-        return reserved.has(extension);
+    private buildDescriptionHtml(): string {
+        let descriptionHtml = "<span class=\"todotxt-item-description\">";
+        for (const str of this.body().split(" ")) {
+            if (str.startsWith(Extension.DUE + ":")) {
+                const split = str.split(":");
+                const due = moment(split.at(1));
+                const now = moment();
+                const extension = split[0] + ":" + split[1];
+                if (due.isSame(now, "d")) {
+                    descriptionHtml += "<span class=\"todotxt-due-ext todotxt-due-today\">" + extension + "</span>";
+                } else if (due.isBefore(now, "d")) {
+                    descriptionHtml += "<span class=\"todotxt-due-ext todotxt-overdue\">" + extension + "</span>";
+                } else {
+                    descriptionHtml += "<span class=\"todotxt-due-ext todotxt-due-later\">" + extension + "</span>";
+                }
+                const remaining = split.slice(2);
+                if (remaining.length) {
+                    descriptionHtml += ":" + split.slice(2).join(":");
+                }
+                descriptionHtml += " ";
+            } else {
+                descriptionHtml += str + " ";
+            }
+        };
+        descriptionHtml += "</span>";
+        
+        return descriptionHtml.trimEnd();
     }
 }
