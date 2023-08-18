c194f9d58150d6109e1aa0b0cef5587cf3c67b36
benjamonnguyen <ben85629@gmail.com>
2023-08-17 18:02:36 -0700

    finish due extension feature
    formats
    - YYYY-MM-DD
    - MM-DD
    - #[dateUnit]
    - dayOfWeek

--- src/model/TodoItem.ts
+++ src/model/TodoItem.ts
@@ -2,8 +2,9 @@ import { Item } from "jstodotxt";
 import { randomUUID } from "crypto";
 import { ActionButton, ActionType, type ViewModel } from ".";
 import { AddModal, EditItemModal } from "src/component";
-import { moment } from "obsidian";
+import { moment, Notice } from "obsidian";
 import { Moment } from "moment";
+import MyPlugin from "src/main";
 
 
 export default class TodoItem extends Item implements ViewModel {
@@ -105,16 +106,6 @@ export default class TodoItem extends Item implements ViewModel {
     private processExtensions() {
 
         this.processDueExtension();
-        //     - <number><[dateUnit]> (ex. 1d)
-        //     - dateUnits: d, w, m, y (defaults to d)
-        //     - 0 = today
-        //   - <alias>
-        //     - aliases: M, Tu, W, Th, F, Sa, Su
-        //   - can be combined
-        //     - 1w2d = 9 days (1 week + 2 days)
-        //     - 2mM = first Monday in 2 months
-        //   - <YYYY-MM-DD> (ex. 1996-08-06)
-        //   - <MM-DD> (ex. 08-06)
     }
 
     private processDueExtension() {
@@ -125,16 +116,63 @@ export default class TodoItem extends Item implements ViewModel {
             console.warn(`Removed duplicate key/value pair for reserved extension: ${key}/${value}`);
         }
         const first = extensions.first();
-        if (first) {
-            let res: Moment = moment(first.value, "YYYY-MM-DD", true);
+        // <YYYY-MM-DD> (ex. 1996-08-06)
+        let res: Moment = moment(first?.value, "YYYY-MM-DD", true);
+        if (first && !res.isValid()) {
+            if (!res.isValid()) {
+                // <MM-DD> (ex. 08-06)
+                res = moment(first.value, "MM-DD", true);
+            }
+            const dateCalculationDetails: string[] = [];
+            if (!res.isValid()) {
+                // <number><[dateUnit]> (ex. 1d)
+                // dateUnits: d, w, m, y
+                // if only number is provided, unit is days (ex. 0 = today)
+                const d = /^\d+$/.exec(first.value)?.first() || /(\d+)d/.exec(first.value)?.at(1);
+                const w = /(\d+)w/.exec(first.value)?.at(1);
+                const m = /(\d+)m/.exec(first.value)?.at(1);
+                const y = /(\d+)y/.exec(first.value)?.at(1);
+                // <dayOfWeek>
+                // M, Tu, W, Th, F, Sa, Su
+                // dayOfWeek must be at the very beginning or end
+                const dayOfWeek = /(^(?:M|Tu|W|Th|F|Sa|Su)|(?:M|Tu|W|Th|F|Sa|Su)$)/.exec(first.value)?.at(1);
+
+                // can be combined
+                // 1w2d = 9 days (1 week + 2 days)
+                // 2mM = first Monday in 2 months
+                // M2m = first Monday in 2 months
+                if (d || w || m || y || dayOfWeek) {
+                    res = moment()
+                        .add(d, "d")
+                        .add(w, "w")
+                        .add(m, "M")
+                        .add(y, "y");
+
+                    if (dayOfWeek){
+                        if (!res.isValid()) res = moment();
+                        res.day(["Su", "M", "Tu", "W", "Th", "F", "Sa"].indexOf(dayOfWeek) + 7);
+                        dateCalculationDetails.push("dayOfWeek: " + dayOfWeek);
+                    }
+                    if (d) dateCalculationDetails.push("d: " + d);
+                    if (w) dateCalculationDetails.push("w: " + w);
+                    if (m) dateCalculationDetails.push("m: " + m);
+                    if (y) dateCalculationDetails.push("y: " + y);
+                }
+            }
+
             if (res.isValid()) {
-                this.setExtension(first.key, res.format("YYYY-MM-DD"));
-                return;
-            } 
-            res = moment(first.value, "MM-DD", true);
-            if (res.isValid()) {
-                this.setExtension(first.key, res.format("YYYY-MM-DD"));
-                return;
+                const dueDate = res.format("YYYY-MM-DD");
+                this.setExtension(first.key, dueDate);
+                let msg = "Due date set to " + dueDate;
+                if (dateCalculationDetails.length) {
+                    msg += `\n(${dateCalculationDetails.join(", ")})`;
+                }
+                new Notice(MyPlugin.NAME + " INFO\n" + msg, 15000);
+            } else {
+                const errMsg = "Invalid value for due extension: " + first.value;
+                console.warn(errMsg);
+                this.removeExtension(first.key, first.value);
+                new Notice(MyPlugin.NAME + " WARNING\n" + errMsg, 15000);
             }
         }
     }
