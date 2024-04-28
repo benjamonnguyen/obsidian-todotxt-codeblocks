4a1710d02d52e0f84d8075b2526521f369ce1786
Benjamin Nguyen <ben85629@gmail.com>
2024-04-28 11:38:27 -0700

    Feature/configurable archive file path (#25)
    * bump version to 1.3.0
    
    * impl configurable archive file path

--- src/event-handler/archive.ts
+++ src/event-handler/archive.ts
@@ -28,10 +28,7 @@ export function archiveOrDeleteCompletedTasksModal(listEl: Element): ConfirmModa
 			app,
 			'Archive completed tasks?',
 			'Completed tasks will be moved to archive.txt',
-			async () =>
-				archiveTasks((item) => item.complete(), listEl).then((items) =>
-					notice(`Moved ${items.length} completed tasks to archive.txt`, Level.INFO),
-				),
+			() => archiveTasks((item) => item.complete(), listEl),
 		);
 	} else if (archiveBehavior === 'delete') {
 		return deleteCompletedTasksModal(listEl);
@@ -43,12 +40,11 @@ export function archiveOrDeleteCompletedTasksModal(listEl: Element): ConfirmModa
 export async function archiveTasks(
 	predicate: (item: TodoItem) => boolean,
 	...listEls: Element[]
-): Promise<TodoItem[]> {
-	const archivedItems: TodoItem[] = [];
-	const archiveFile =
-		(app.vault.getAbstractFileByPath('archive.txt') as TFile) ||
-		(await app.vault.create('archive.txt', ''));
+): Promise<void> {
+	const f = await archivalFile();
+	if (!f) return;
 
+	const archivedItems: TodoItem[] = [];
 	listEls.forEach((el) => {
 		const { from, to, todoList } = TodoList.from(el);
 		archivedItems.push(...todoList.removeItems(predicate));
@@ -57,7 +53,7 @@ export async function archiveTasks(
 		}
 	});
 
-	app.vault.process(archiveFile, (data) => {
+	app.vault.process(f, (data) => {
 		const res: string[] = [];
 		if (data) {
 			res.push(data);
@@ -67,7 +63,35 @@ export async function archiveTasks(
 		return res.join('\n');
 	});
 
-	return archivedItems;
+	notice(`Moved ${archivedItems.length} completed task(s) to ${f.path}`, Level.INFO);
+}
+
+async function archivalFile(): Promise<TFile | undefined> {
+	const path = SETTINGS_READ_ONLY.archiveFilePath;
+
+	// must use txt extension
+	if (!path.endsWith('.txt')) {
+		notice('archive file path must use txt extension', Level.ERR, 10000);
+		return;
+	}
+
+	//
+	const file = app.vault.getAbstractFileByPath(path) as TFile;
+	if (file) return file;
+
+	// create folders if not exist
+	const lastSlash = path.lastIndexOf('/');
+	if (lastSlash >= 0) {
+		await app.vault.createFolder(path.substring(0, lastSlash));
+	}
+
+	// create file
+	try {
+		return await app.vault.create(path, '');
+	} catch (e) {
+		console.error(e);
+		notice('invalid archive file path: ' + path, Level.ERR, 10000);
+	}
 }
 
 export function autoArchive(mdView: MarkdownView | null) {
@@ -78,18 +102,16 @@ export function autoArchive(mdView: MarkdownView | null) {
 	const predicate = (item: TodoItem) =>
 		item.completed() !== null &&
 		moment().diff(moment(item.completed()), 'd') >= autoArchiveThreshold;
-	let removedTasks: Promise<TodoItem[]>;
 	const archiveBehavior = SETTINGS_READ_ONLY.archiveBehavior;
 	if (archiveBehavior === 'archive') {
-		removedTasks = archiveTasks(predicate, ...listEls);
+		archiveTasks(predicate, ...listEls);
 	} else if (archiveBehavior === 'delete') {
-		removedTasks = deleteTasks(predicate, ...listEls);
+		deleteTasks(predicate, ...listEls).then((items) => {
+			if (items) {
+				notice(`Auto-deleted ${items.length} completed task(s)`, Level.INFO);
+			}
+		});
 	} else {
 		throw new Error('No implementation for archiveBehavior: ' + archiveBehavior);
 	}
-	removedTasks.then((items) => {
-		if (items.length) {
-			notice(`Auto-archived ${items.length} items`, Level.INFO);
-		}
-	});
 }
